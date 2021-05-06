7be943f2e9bc668e6cb836163702bd5ba870a0f5
Max <Max2827@yande.ru>
2021-05-06 15:57:47 +0300

    list

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
             System.out.println("Hello guys!");
         }
+        List<Integer> arr = new ArrayList<>();
     }
 }
