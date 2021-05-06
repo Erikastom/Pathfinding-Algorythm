92c9f829b144601c5ace2460db97255a985baabb
Unicorn <svyatoslav.raklov@bk.ru>
2021-05-06 16:23:53 +0300

    T

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 4; i++) {
-            System.out.println("Hello guys!");
-        }
+        int a = 5;
+        int b = 5;
+        System.out.println(a == b);
+        Integer c = 10;
+        Integer d = 10;
+        System.out.println(c == d);
+        Integer e = 128;
+        Integer f = 128;
+        System.out.println(e == f);
     }
 }
